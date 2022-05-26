d6a74d310a33134d48c99ec2df2e88e8c76e59e9
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-05-26 12:38:20 +0300

    created controls component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Controls } from './components/controls';
 import { Header } from './components/header';
 
 function App() {
 	return (
 		<>
 			<Header />
+      <Controls/>
 		</>
 	);
 }
