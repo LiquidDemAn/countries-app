7f53cbdc492197df459fb16c639e759a29de1dfd
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-05-26 11:51:26 +0300

    created header component

---
+++ src/components/header/header.styled.ts
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+import { Link } from 'react-router-dom';
+
+export const HeaderEl = styled.header`
+	box-shadow: var(--shadow);
+	background-color: var(--colors-ui-base);
+`;
+
+export const HeaderWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	padding: 2rem 0;
+`;
+
+export const HeaderTitle = styled.a.attrs({
+	to: '/',
+})`
+	color: var(--colors-text);
+	font-size: var(--fs-sm);
+	text-decoration: none;
+	font-weight: var(--fw-bold);
+`;
+
+// export const HeaderTitle = styled(Link).attrs({
+// 	to: '/',
+// })`
+// 	color: var(--colors-text);
+// 	font-size: var(--fs-sm);
+// 	text-decoration: none;
+// 	font-weight: var(--fw-bold);
+// `;
+
+// export const HeaderTitle = styled.h1``;
+
+export const ModeSwitcher = styled.div`
+	display: flex;
+	align-items: center;
+	column-gap: 0.75rem;
+	color: var(--colors-text);
+	font-size: var(--fs-sm);
+	font-weight: var(--fw-bold);
+    text-transform: capitalize;
+	cursor: pointer;
+`;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,37 @@
+import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+import { useState, useEffect } from 'react';
+import { Container } from '../container';
+import {
+	HeaderEl,
+	HeaderWrapper,
+	HeaderTitle,
+	ModeSwitcher,
+} from './header.styled';
+
+type ThemeType = 'light' | 'dark';
+
+export const Header = () => {
+	const [theme, setTheme] = useState<ThemeType>('light');
+
+	const toggleTheme = () => {
+		setTheme(theme === 'light' ? 'dark' : 'light');
+	};
+
+	useEffect(() => {
+		document.body.setAttribute('data-theme', theme);
+	}, [theme]);
+
+	return (
+		<HeaderEl>
+			<Container>
+				<HeaderWrapper>
+					<HeaderTitle>Where is the world?</HeaderTitle>
+					<ModeSwitcher onClick={toggleTheme}>
+						{theme === 'light' ? <IoMoonOutline /> : <IoMoon />}
+						<span>{theme} Theme</span>
+					</ModeSwitcher>
+				</HeaderWrapper>
+			</Container>
+		</HeaderEl>
+	);
+};
