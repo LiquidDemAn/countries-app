29942a9664e761a7bea897ea4a606df617a01cc0
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-05-28 14:07:17 +0300

    created loadAllCountries action

---
+++ src/pages/homepage/services/actions.ts
@@ -0,0 +1,12 @@
+import { ALL_COUNTRIES } from './../../../config';
+import { AllCountriesType } from './typedef';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export const loadAllCountries = createAsyncThunk<AllCountriesType>(
+	'homepage/load-all-countries',
+	async () => {
+		const response = await axios.get(ALL_COUNTRIES);
+		return await response.data;
+	}
+);
