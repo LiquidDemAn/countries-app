543c52c75e47fef4fd179d6f5b245414182ea091
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-05-28 17:35:30 +0300

    changed countries-list styles

--- src/components/countries-list/countries-list.styled.ts
+++ src/components/countries-list/countries-list.styled.ts
@@ -4,9 +4,8 @@ export const CountriesListWrapper = styled.section`
 	width: 100%;
 	display: flex;
 	flex-wrap: wrap;
-	justify-content: space-between;
 	row-gap: 30px;
-	column-gap: 30px;
+	column-gap: 58.5px;
 
 	@media (min-width: 767px) {
 	}
