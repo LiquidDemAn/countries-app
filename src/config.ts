e6226339f6137b338487c35aa5d205260a67667d
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-05-28 13:55:11 +0300

    created config with ursl

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = 'https://restcountries.com/v2/';
+export const ALL_COUNTRIES = `${BASE_URL}all?fields=name,capital,region,population,flags`;
+export const COUNTRY = (name: string) => {
+	return `${BASE_URL}name/${name}?fields=name,nativeName,capital,population,flags,region,subregion,topLevelDomain,currencies,languages,borders`;
+};
