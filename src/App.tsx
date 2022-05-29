406afa9eeab50ce92dc0b69ce011a3824d8440c4
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-05-29 09:41:42 +0300

    created not-found-page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { HomePage } from './pages/home-page';
+import { NotFoundPage } from './pages/not-found-page';
+import { CountryPage } from './pages/country-page';
 
 function App() {
 	return (
 		<Provider store={store}>
 			<Routes>
 				<Route path='/' element={<HomePage />} />
+				<Route path='/country/:name' element={<CountryPage />} />
+				<Route path='*' element={<NotFoundPage />} />
 			</Routes>
 		</Provider>
 	);
