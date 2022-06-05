14652aa228e7d0030b88d75c5296162a2fb4cce8
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-06-05 13:25:09 +0300

    finished not-found page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,3 +1,12 @@
+import { Link } from 'react-router-dom';
+import { Main } from '../../components/main';
+
 export const NotFoundPage = () => {
-	return <>NotFound</>;
+	return (
+		<Main>
+			<>
+				Page not found. Back to <Link to='/'>Homepage</Link>
+			</>
+		</Main>
+	);
 };
