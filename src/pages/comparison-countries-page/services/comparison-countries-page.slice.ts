3aa8acb38789625c417c1b1d651f7abbb0b96456
NoDiggity.Dexter <dimonshinkarenko2001@gmail.com>
2022-06-19 19:01:07 +0300

    added loader for comparison countirs page

--- src/pages/comparison-countries-page/services/comparison-countries-page.slice.ts
+++ src/pages/comparison-countries-page/services/comparison-countries-page.slice.ts
@@ -2,7 +2,9 @@ import { createSlice } from '@reduxjs/toolkit';
 import { loadFirstCountry, loadSecondCountry } from './actions';
 import { ComparisonCountriesPageStateType } from './typedef';
 
-const initialState: ComparisonCountriesPageStateType = {};
+const initialState: ComparisonCountriesPageStateType = {
+	loading: false,
+};
 
 export const comparisonCountriesPageSlice = createSlice({
 	name: 'comparison-countries-page',
@@ -10,10 +12,18 @@ export const comparisonCountriesPageSlice = createSlice({
 	reducers: {},
 	extraReducers: (buidder) =>
 		buidder
+			.addCase(loadFirstCountry.pending, (state) => {
+				state.loading = true;
+			})
 			.addCase(loadFirstCountry.fulfilled, (state, { payload }) => {
 				state.firstCountry = payload;
+				state.loading = false;
+			})
+			.addCase(loadSecondCountry.pending, (state) => {
+				state.loading = true;
 			})
 			.addCase(loadSecondCountry.fulfilled, (state, { payload }) => {
 				state.secondCountry = payload;
+				state.loading = false;
 			}),
 });
--- src/pages/comparison-countries-page/services/selectors.ts
+++ src/pages/comparison-countries-page/services/selectors.ts
@@ -1,8 +1,13 @@
 import { AppState } from './../../../store/typedef';
 
+export const getComparisonCountriesPageLoading = (state: AppState) => {
+	return state.comparisonCountriesPage.loading;
+};
+
 export const getFirstCountry = (state: AppState) => {
 	return state.comparisonCountriesPage.firstCountry;
 };
+
 export const getSecondCountry = (state: AppState) => {
 	return state.comparisonCountriesPage.secondCountry;
 };
